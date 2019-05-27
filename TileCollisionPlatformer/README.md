238914570e6c9a287b8bf70abf79cc4319ae616d
nezvers <agnis16@inbox.lv>
2019-05-27 19:23:34 +0300

    First commit
    Uploaded "True Tile Collision" example

---
+++ TileCollisionPlatformer/README.md
@@ -0,0 +1,11 @@
+# Welcome to "True Tile Collision" example project
+
+This is example project and will be updated as "True Tile Collision" is developed.
+
+Physics is contained in ["true_tile_collision.lua"](defold://open?path=/true_tile_collision/true_tile_collision.lua). It is applied to ["player.script"](defold://open?path=/creatures/player/player.script).
+
+Check out GitHub for the ["True Tile Collision"](https://github.com/nezvers/true-tile-collision).
+
+Happy Defolding!
+
+---
---
+++ TileCollisionPlatformer/creatures/player/player.go
@@ -0,0 +1,35 @@
+components {
+  id: "player"
+  component: "/creatures/player/player.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/creatures/player/blocky.atlas\"\n"
+  "default_animation: \"idle\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 7.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
